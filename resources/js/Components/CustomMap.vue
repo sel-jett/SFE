58659fdcc30396f3c6cb2962fd41385216bd130b
salah <eljettiouisalaheddine@gmail.com>
2023-04-17 22:56:40 +0000

    error user Location

--- resources/js/Components/CustomMap.vue
+++ resources/js/Components/CustomMap.vue
@@ -6,52 +6,31 @@
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "../../../public/leaflet.curve.js";
+import { onMounted } from 'vue';
 
 export default {
   name: "LeafletMap",
-  data() {
-    return {
-      map: null,
-    };
-  },
-  mounted() {
-    this.map = L.map("mapContainer").setView([46.05, 11.05], 5);
-    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
-    //use a mix of renderers
-    var customPane = this.map.createPane("customPane");
-    var canvasRenderer = L.canvas({ pane: "customPane" });
-    customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
-    L.marker([50, 14]).addTo(this.map);
-
-    L.marker([53, 20]).addTo(this.map);
-    L.marker([49.5, 19.5]).addTo(this.map);
-    L.marker([49, 25]).addTo(this.map);
-    L.marker([-10, 25]).addTo(this.map);
-    L.marker([10, -25]).addTo(this.map);
-    L.marker([0, 0]).addTo(this.map);
-    var pathOne = L.curve(["M", [50, 14], "Q", [53, 20], [49, 25]], {
-      renderer: canvasRenderer,
-    }).addTo(this.map);
-    L.curve(["M", [50, 14], "Q", [52, 20], [49, 25]], {
-      renderer: canvasRenderer,
-    }).addTo(this.map);
-    L.curve(["M", [50, 14], "Q", [51, 20], [49, 25]], {
-      renderer: canvasRenderer,
-    }).addTo(this.map);
-    L.curve(["M", [50, 14], "Q", [50, 20], [49, 25]], {
-      renderer: canvasRenderer,
-    }).addTo(this.map);
-    L.curve(["M", [50, 14], "Q", [47, 20], [49, 25]], {
-      renderer: canvasRenderer,
-    }).addTo(this.map);
-  },
-  onBeforeUnmount() {
-    if (this.map) {
-      this.map.remove();
-    }
-  },
+  setup () {
+    let mymap;
+    onMounted(() => {
+      mymap = L.map('mapContainer').setView([51.505, -0.09], 13);
+      L
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FsYWhqZXR0aW91aSIsImEiOiJjbGdsMXgxYzEwNXdpM2VxaWdneDRhZHN3In0.9LkUVnRagdHkMm1vXWnmyg",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1Ijoic2FsYWhqZXR0aW91aSIsImEiOiJjbGdsMXgxYzEwNXdpM2VxaWdneDRhZHN3In0.9LkUVnRagdHkMm1vXWnmyg",
+          }
+          
+        )
+        .addTo(mymap);
+    });
+  }
 };
 </script>
